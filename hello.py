2bbb5866cec1bf2abb4252ad4fdd464232871189
sushmai <maryadareddy@gmail.com>
2019-01-30 10:35:48 -0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
+# Importing Flask class 
 from flask import Flask
+# create app variable and set it to the instance of app class.
+#__name__ == name of the module (flask knows where to look for)
 app = Flask(__name__)
 
+# route where to go on web page
+# route = decorator(to add additional functionality to existing function)
+# "/" = home page 
 @app.route("/")
-def hello():
-    return "<h1>Hello World!<h1>"
+@app.route("/home")
+def home():
+    # what you see on page 
+    return "<h1>Home Page...!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page..</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
